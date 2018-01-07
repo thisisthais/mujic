ed4232cc8666301103a9b7c59742a5110807d087
Thais Laney <thaisc@fb.com>
2018-01-06 21:44:51 -0500

    Max code review

--- midi-parser.clj
+++ midi-parser.clj
@@ -10,26 +10,23 @@
    ShortMessage/POLY_PRESSURE    :poly-pressure
    ShortMessage/PROGRAM_CHANGE   :program-change})
 
-(def key-names '("C" "C#" "D" "D#" "E" "F" "F#" "G" "G#" "A" "A#" "B"))
+(def key-names ["C" "C#" "D" "D#" "E" "F" "F#" "G" "G#" "A" "A#" "B"])
 
-(defn parse-midi-file [filepath]
-  "Given a midi file, outputs a human-readable collection of parsed event data"
-  (let [sequence (-> (io/file filepath) MidiSystem/getSequence)
-        tracks (.getTracks sequence)
-        events (get-all-events tracks)]
-    events))
+(defn note-to-string
+  "Given a midi note number, returns the note name."
+  [num]
+  (let [note (rem num 12)
+        octave (quot num 12)]
+    (str (nth key-names note) octave)))
 
-(defn get-all-events
-  "Given an array of tracks, returns a collection of parsed events per track"
-  [tracks]
-  (map get-track-events tracks))
-
-(defn get-track-events
-  "Given a track, returns a collection of its parsed events"
-  [track]
-  (let [size (.size track)
-        index (take size (iterate inc 0))]
-    (map #(parse-event (.get track %)) index)))
+(defn parse-message
+  "Given an event and its ShortMessage, parses useful information into human-readable format"
+  [message, event]
+  {:tick (.getTick event)
+   :command (get command-map (.getCommand message))
+   :channel (.getChannel message)
+   :note (note-to-string (.getData1 message))
+   :velocity (.getData2 message)})
 
 (defn parse-event
   "Given an event, returns the parsed event data if the event does not represet metadata"
@@ -38,19 +35,22 @@
     (if (instance? ShortMessage message)
       (parse-message (cast ShortMessage message) event))))
 
-(defn parse-message
-  "Given an event and its ShortMessage, parses useful information into human-readable format"
-  [message, event]
-  (let [tick (.getTick event)
-        command (get command-map (.getCommand message))
-        channel (.getChannel message)
-        note (note-to-string (.getData1 message))
-        velocity (.getData2 message)]
-    {:tick tick :command command :channel channel :note note :velocity velocity}))
+(defn get-track-events
+  "Given a track, returns a collection of its parsed events"
+  [track]
+  (let [size (.size track)
+        index (range size)]
+    (map #(parse-event (.get track %)) index)))
 
-(defn note-to-string
-  "Given a MIDI note number, returns the note name."
-  [num]
-  (let [note (rem num 12)
-        octave (quot num 12)]
-    (str (nth key-names note) octave)))
+(defn get-all-events
+  "Given an array of tracks, returns a collection of parsed events per track"
+  [tracks]
+  (map get-track-events tracks))
+
+(defn parse-midi-file [filepath]
+  "Given a midi file, outputs a human-readable collection of parsed event data"
+  (let [sequence (MidiSystem/getSequence (io/file filepath))
+        tracks (.getTracks sequence)
+        resolution (.getResolution sequence) ;; gonna need later
+        events (get-all-events tracks)]
+    events))
